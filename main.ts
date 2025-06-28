1b0fae98cdb596c743047313067f524a99c7fd0b
Taras Glek <taras@glek.net>
2025-06-28 14:02:58 +0300

    misc

--- main.ts
+++ main.ts
@@ -1,8 +1,36 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { Agent, run } from "@openai/agents";
+
+const params = {
+  model: "gpt-4.1-nano",
+};
+
+const historyTutorAgent = new Agent({
+  ...params,
+  name: "History Tutor",
+  instructions:
+    "You provide assistance with historical queries. Explain important events and context clearly.",
+});
+
+const mathTutorAgent = new Agent({
+  ...params,
+  name: "Math Tutor",
+  instructions:
+    "You provide help with math problems. Explain your reasoning at each step and include examples",
+});
+
+const triageAgent = new Agent({
+  ...params,
+  name: "Triage Agent",
+  instructions:
+    "You determine which agent to use based on the user's homework question",
+  handoffs: [historyTutorAgent, mathTutorAgent],
+});
+
+async function main() {
+  const result = await run(triageAgent, "What is the capital of France?", {
+    stream: true,
+  });
+  console.log(result.finalOutput);
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+main().catch((err) => console.error(err));
