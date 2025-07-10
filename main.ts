f094b72ea8f3b22169247bdeba7f475ab60b426f
Taras Glek <taras@glek.net>
2025-07-10 17:31:40 +0300

    fix: Adjust redirect anchor and pre tag style

--- main.ts
+++ main.ts
@@ -120,7 +120,7 @@ app.get("/:currentChatId", (c) => {
       let oldWipMsg = '';
       // so that the screen doesn't jump around
       // 50vh = 50% of viewport height
-      await stream.write(html`<p style="min-height: 50vh;"><a name="${(await model.get()).length}"></a><pre>`.toString())
+      await stream.write(html`<p><a name="${(await model.get()).length}"></a><pre  style="min-height: 50vh;">`.toString())
       while (model.wipMsg !== null) {
         if (oldWipMsg === model.wipMsg) {
           await stream.sleep(1000 / 60); // 60 FPS
@@ -160,7 +160,7 @@ app.post("/:currentChatId", async (c) => {
     content: userInput.trim(),
   } as AgentInputItem;
   await model.appendMessages([msg]);
-  return c.redirect(`/${currentChatId}#${(await model.get()).length}`);
+  return c.redirect(`/${currentChatId}#${(await model.get()).length - 1}`);
 });
 
 export default app;
