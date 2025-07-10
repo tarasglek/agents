7f8b2e6dfb852092c3fe6f6929095979cbc331b9
Taras Glek <taras@glek.net>
2025-07-10 13:21:50 +0300

    feat: Add stringifyYaml utility function

---
+++ util.ts
@@ -0,0 +1,15 @@
+import { stringify } from "jsr:@std/yaml";
+
+export function stringifyYaml(obj: unknown): string {
+    try {
+        obj = obj.map(msg => {
+            let role = msg.role;
+            if (role === "assistant") {
+                role = "🤖 " + role
+            }
+            return { ...msg, role };
+        });
+    } catch (_e) {
+    }
+    return stringify(obj, { skipInvalid: true });
+}
